67ac9af6efdaa752721c61bf9a3915407650ffa6
JoseChirinos <josealexcba@gmail.com>
2018-08-17 18:23:38 -0400

    images fixed

--- js/random.js
+++ js/random.js
@@ -1,7 +1,7 @@
 var datos = [
   {
     "id": "5b76647985e4245a873eeff9",
-    "image": "/images/samsung.png",
+    "image": "images/samsung.png",
     "name": "Samsung S9",
     "description": "Elit irure esse ut veniam non minim ut velit esse irure qui laboris consequat. Dolore irure dolore incididunt id Lorem esse voluptate ea id exercitation nisi. Ut adipisicing veniam qui nostrud duis ad aliqua magna excepteur reprehenderit. Elit qui minim officia consequat sunt.\r\n",
     "data_start": "2014-04-29 10:40:18",
@@ -9,7 +9,7 @@ var datos = [
   },
   {
     "id": "5b766479d94db87d199ff751",
-    "image": "/images/iphone-x.jpg",
+    "image": "images/iphone-x.jpg",
     "name": "iPhone X",
     "description": "Cillum irure duis reprehenderit id officia sunt. Esse sint eiusmod ex nostrud anim ut. Excepteur enim exercitation excepteur excepteur. Quis amet deserunt incididunt aliqua eiusmod quis ea proident est occaecat exercitation voluptate tempor deserunt. Sit sint nostrud id occaecat enim magna laborum. Cillum esse dolor nulla nisi aliquip quis id pariatur qui duis deserunt. Ex nisi sunt quis cupidatat exercitation proident.\r\n",
     "data_start": "2014-06-09 08:05:28",
@@ -17,7 +17,7 @@ var datos = [
   },
   {
     "id": "5b76647942b10ad96c4fe22c",
-    "image": "/images/huawei-20.jpg",
+    "image": "images/huawei-20.jpg",
     "name": "Huawei P20",
     "description": "Veniam commodo sunt dolor irure ipsum Lorem deserunt dolore quis ullamco quis. In dolore officia irure in laboris sint ipsum. Proident sunt commodo consequat voluptate mollit incididunt. Occaecat nulla sit ullamco ut veniam ullamco excepteur ex. Proident commodo aliqua proident enim Lorem cillum mollit enim incididunt consectetur sint eu labore. Commodo minim Lorem aliquip labore adipisicing id.\r\n",
     "data_start": "2017-09-25 12:06:12",
@@ -25,7 +25,7 @@ var datos = [
   },
   {
     "id": "5b7664791a84e31d67df8ac2",
-    "image": "/images/sony-x.jpg",
+    "image": "images/sony-x.jpg",
     "name": "Sony Xperia XZ2",
     "description": "Proident ea ad do non Lorem. Consectetur officia deserunt irure irure deserunt velit minim eu veniam. In deserunt officia esse est ipsum ea cillum reprehenderit cillum irure. Reprehenderit voluptate aliquip minim officia exercitation ullamco ea et veniam proident magna. Sit nulla nostrud et id ipsum eu quis ipsum laborum veniam. Duis irure mollit Lorem reprehenderit cupidatat irure veniam ut id id laboris reprehenderit.\r\n",
     "data_start": "2016-09-30 12:07:10",
@@ -33,7 +33,7 @@ var datos = [
   },
   {
     "id": "5b76647985e4245a873eeff9",
-    "image": "/images/s8.jpg",
+    "image": "images/s8.jpg",
     "name": "Samsung S8 Plus",
     "description": "Elit irure esse ut veniam non minim ut velit esse irure qui laboris consequat. Dolore irure dolore incididunt id Lorem esse voluptate ea id exercitation nisi. Ut adipisicing veniam qui nostrud duis ad aliqua magna excepteur reprehenderit. Elit qui minim officia consequat sunt.\r\n",
     "data_start": "2014-04-29 10:40:18",
@@ -41,7 +41,7 @@ var datos = [
   },
   {
     "id": "5b766479d94db87d199ff751",
-    "image": "/images/one5.jpg",
+    "image": "images/one5.jpg",
     "name": "One Plus 5",
     "description": "Cillum irure duis reprehenderit id officia sunt. Esse sint eiusmod ex nostrud anim ut. Excepteur enim exercitation excepteur excepteur. Quis amet deserunt incididunt aliqua eiusmod quis ea proident est occaecat exercitation voluptate tempor deserunt. Sit sint nostrud id occaecat enim magna laborum. Cillum esse dolor nulla nisi aliquip quis id pariatur qui duis deserunt. Ex nisi sunt quis cupidatat exercitation proident.\r\n",
     "data_start": "2014-06-09 08:05:28",
@@ -49,7 +49,7 @@ var datos = [
   },
   {
     "id": "5b76647942b10ad96c4fe22c",
-    "image": "/images/nokia6.png",
+    "image": "images/nokia6.png",
     "name": "Nokia 6",
     "description": "Veniam commodo sunt dolor irure ipsum Lorem deserunt dolore quis ullamco quis. In dolore officia irure in laboris sint ipsum. Proident sunt commodo consequat voluptate mollit incididunt. Occaecat nulla sit ullamco ut veniam ullamco excepteur ex. Proident commodo aliqua proident enim Lorem cillum mollit enim incididunt consectetur sint eu labore. Commodo minim Lorem aliquip labore adipisicing id.\r\n",
     "data_start": "2017-09-25 12:06:12",
@@ -57,7 +57,7 @@ var datos = [
   },
   {
     "id": "5b7664791a84e31d67df8ac2",
-    "image": "/images/nokia1100.jpg",
+    "image": "images/nokia1100.jpg",
     "name": "Nokia 1100",
     "description": "Proident ea ad do non Lorem. Consectetur officia deserunt irure irure deserunt velit minim eu veniam. In deserunt officia esse est ipsum ea cillum reprehenderit cillum irure. Reprehenderit voluptate aliquip minim officia exercitation ullamco ea et veniam proident magna. Sit nulla nostrud et id ipsum eu quis ipsum laborum veniam. Duis irure mollit Lorem reprehenderit cupidatat irure veniam ut id id laboris reprehenderit.\r\n",
     "data_start": "2016-09-30 12:07:10",
